d378db78775bc154deb10c27723df1d5044df7de
chen-blips <mjrpalacol@gmail.com>
2025-11-29 00:34:17 +0800

    Update photoGallery.php

--- photoGallery.php
+++ photoGallery.php
@@ -21,7 +21,7 @@
         </div>
         <nav class="nav-bar-casing">
           <ul class="nav-bar">
-              <li><a href="home.php">Home</a></li>
+              <li><a href="index.html">Home</a></li>
               <li class="separator"></li>
               <li><a href="about.php">About</a></li>
               <li class="separator"></li>
@@ -704,3 +704,4 @@
 </body>
 
 </html>
+
